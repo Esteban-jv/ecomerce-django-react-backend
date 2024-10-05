9bde9e5a341a01c57a5dde894530405fa03fedc6
Esteban-jv <ejv.developer@gmail.com>
2024-10-05 16:32:00 -0500

    Just a little bit of inprovement on admin dashboard

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import User, Profile
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['email','username','first_name','last_name']
+    search_fields = ['email','username','phone','first_name','last_name']
+    list_filter = ['is_active','is_staff','is_superuser']
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
